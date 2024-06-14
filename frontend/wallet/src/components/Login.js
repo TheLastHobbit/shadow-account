7257e2a6b3860a02c1e85c61c1c12092df073d87
hboolean <1007504655@qq.com>
2024-06-14 23:46:50 +0800

    add config

--- frontend/wallet/src/components/Login.js
+++ frontend/wallet/src/components/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link,useNavigate } from 'react-router-dom';
 import Button from './Button';
 import '../css/Login.css'
+import axios from 'axios';
 
 function Login() {
     const [email, setEmail] = useState('');
